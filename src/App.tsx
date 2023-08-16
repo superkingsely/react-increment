89d665582714823843584657d289e34bcfd0745e
chijioke <superkingsely@gmail.com>
2023-08-16 08:29:34 -0700

    okay

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { ReactNode } from "react"
 
 const App = () => {
+    const listofplayer=['messi','ronaldo','kaka']
   return (
-    <div>App</div>
+    <div>
+        {
+            listofplayer.map(function(player){
+                return(
+                    <div>
+                        <h1>{player}</h1>
+                    </div>
+                )
+            })
+        }
+        <ul>
+
+        {
+            listofplayer.map(player=>{
+                return(
+                    <li><h1>{player}</h1></li>
+                )
+            })
+        }
+        </ul>
+    </div>
   )
 }
 
