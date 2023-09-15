e6c23eb0b95961f887725b9ff24e2040a30f982e
chijioke <superkingsely@gmail.com>
2023-09-15 01:34:32 -0700

    okaydone here

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import { ReactNode } from "react"
+import React, { useState } from "react";
 
 const App = () => {
-    const listofplayer=['messi','ronaldo','kaka']
+    const[count,setCount]=useState(0)
+    const handleincrement=()=>{
+        setCount(prev=>prev + 1)
+    }
   return (
-    <div>
-        {
-            listofplayer.map(function(player){
-                return(
-                    <div>
-                        <h1>{player}</h1>
-                    </div>
-                )
-            })
-        }
-        <ul>
-
-        {
-            listofplayer.map(player=>{
-                return(
-                    <li><h1>{player}</h1></li>
-                )
-            })
-        }
-        </ul>
+    <div
+      className="app"
+      style={{ minHeight: "100vh", backgroundColor: "yellow" }}
+    >
+      <br />
+      <div className="container" style={{border:'2px solid'}}>
+        <div className="screen" style={{ backgroundColor: "gray" }}>
+          <h1>{count}</h1>
+        </div>
+        <div className="action">
+          <button onClick={()=>{setCount(0)}}>refresh</button>
+          <button onClick={()=>{setCount(prev=>prev -1)}}>Drecrement</button>
+          <button
+          onClick={handleincrement}
+          >increment</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
